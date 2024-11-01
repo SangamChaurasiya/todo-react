2c35252c1321b39edae37772f20ec97dc1ac98ad
SangamChaurasiya <sangam.chaurasiya0604@gmail.com>
2024-11-01 12:01:41 +0530

    bind input with enter key

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -47,7 +47,10 @@ function Todo() {
     return (
         <div className='todo-container'>
             <h2>Todo List</h2>
-            <input type="text" value={inputValue} onChange={(e) => setInputValue(e.target.value)}/>
+            <input type="text" value={inputValue} onChange={(e) => setInputValue(e.target.value)} onKeyDown={(e) => {
+                if (e.key === "Enter")
+                    addTodo();
+            }} />
             {
                 editMode ? (
                     <div>
